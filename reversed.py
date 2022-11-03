e3a317044d3949ca104e87721f5f501523b87976
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-03 20:18:21 +0300

    Gave more details to the reverse process.

--- reversed.py
+++ reversed.py
@@ -1,9 +1,23 @@
-num_list = []
+#############################################################################
+## 									   ##
+##	Title : Reverse a  List 		 			   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
 
-n = 10
+# Build a function that returns an array of integers from n to 1 where n>0.
 
-for i in range(n):
-	num_list.append(i)
-	print(i)
-	print(num_list)
-print(num_list)
+# Example : n=5 --> [5,4,3,2,1]
+
+def reverse_seq(n):
+	num_list = [] #1. Decided to create an empty list.
+	
+	# The idea is to loop through the give range in n; the range should start from 1 all the way to n
+	# with the range fun however it usually never outputs the last expected digit, so I added 1 to n.
+
+	for i in range(1, n + 1):
+		num_list.append(i) # For each iteration/loop add a number to the list.
+	num_list.reverse() # Now reverse the list last #no to be first and first to be last.
+	return num_list
