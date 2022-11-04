899743f22ecc64beb4f7f0caebf3415f25bc8f98
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 20:49:33 +0300

    Did the power arrays challenge.

---
+++ power_arrays.py
@@ -0,0 +1,9 @@
+num_list = []
+
+n = 0
+
+for i in range(n+1):
+	print(i)
+	x = i ** 2
+	print(x)
+	num_list.append(x)
---
+++ sum_arrays.py
@@ -0,0 +1,14 @@
+#############################################################################
+## 									   ##
+##	Title : Sum Arrays						   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+
+# Basically look for the sum of an array and it should return 0 if array is [].
+
+def sum_array(a):
+	return sum(a) b = [] if a == b : return 0 else:
