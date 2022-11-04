773fb87f61d4edd87013fcaea7da473248dc5bc7
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 20:50:30 +0300

    Did the power arrays challenge.

--- power_arrays.py
+++ power_arrays.py
@@ -1,9 +1,20 @@
+#############################################################################
+## 									   ##
+##	Title :  Power Array						   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+
+# This was a simple one, loop through a given range and have each value power 2, the result should be resulted in form of a list.
+
 num_list = []
-
-n = 0
-
 for i in range(n+1):
-	print(i)
-	x = i ** 2
-	print(x)
+	x = 2 ** i
 	num_list.append(x)
+return num_list
+
+# A clever way I saw was:
+#	return [2**i for i in range(n+1)]
