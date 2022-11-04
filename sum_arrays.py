153ae46155d53936464bd74a0a04d6157cc48a8b
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 20:51:07 +0300

    Did the power arrays challenge.

--- sum_arrays.py
+++ sum_arrays.py
@@ -11,4 +11,16 @@
 # Basically look for the sum of an array and it should return 0 if array is [].
 
 def sum_array(a):
-	return sum(a) b = [] if a == b : return 0 else:
+    b = []
+    if a == b:
+        return 0
+    else: 
+        return sum(a)
+        
+# 1 Thing I didnt realise was an empty array would return 0 if sum is done.
+# Some of the clever ways I saw.
+# 	return sum(a) if a !=0 else 0
+
+#	return sum(a)
+
+
