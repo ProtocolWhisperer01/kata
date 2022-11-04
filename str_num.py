1fc04d9357a4e7e5d1fd428012429f1fa960bd91
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 15:28:15 +0300

    Did the string to number challenge.

---
+++ str_num.py
@@ -0,0 +1,18 @@
+#############################################################################
+## 									   ##
+##	Title : String to Number					   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# This challenge was basically a str number into an integer type
+
+def string_to_number(s): # for the string input
+	num = int(s) # change the type from str to int
+	return num
+
+'''
+	return int(s) ---> is one clever way I saw.
+'''
