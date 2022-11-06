2e577c37d35ed7c3e0cbb58aff5427d368a82799
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-07 00:13:57 +0300

    Did the omit the first and last char challenge.

---
+++ remove_first_last_char.py
@@ -0,0 +1,16 @@
+#############################################################################
+## 									   ##
+##	Title : Remove First and Last character				   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# As the title reads remove the first and last character
+
+def string(s):
+	return s[1:len(s)-1]
+
+
+# a simpler way was return s [1:-1]
