5e0e154ed91572e2fb366055f66ab1129234891d
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-07 23:26:22 +0300

    Did the first regex challenge.

---
+++ regex_1.py
@@ -0,0 +1,16 @@
+#############################################################################
+## 									   ##
+##	Title : Regex 1							   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# Working on regular expressions.
+# Simple validation with regex.
+
+import re
+
+def valid_user(username):
+	
