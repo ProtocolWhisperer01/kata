ed7942a758d7a14cdcc88e12706ef72e8f0e5f0f
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-11 23:28:52 +0300

    Did the rem_exclamation challenge.

---
+++ rem_exclamation.py
@@ -0,0 +1,16 @@
+#############################################################################
+## 									   ##
+##	Title : Remove the exclamation 					   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# In this challenge I am expected to remove the exclamtion mark from a string.
+# This was pretty straight forward.
+
+def rem_excl(s):
+	return s.replace('!','')
+
+# So just replace the '!' with an empty string.
