34084dd1dc403f7c2dc795a0959b68766ff1bb21
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-11 23:56:28 +0300

    Did the multi_list challenge.

---
+++ multi_list.py
@@ -0,0 +1,8 @@
+#############################################################################
+## 									   ##
+##	Title : Multiplication Of Items in a list			   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
