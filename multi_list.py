c04787e6345423c7e2fe553c2f81f5e715c72984
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-11 23:58:42 +0300

    Did some changes on the file.

--- multi_list.py
+++ multi_list.py
@@ -6,3 +6,10 @@
 ##									   ##
 ##	Github : https://github.com/ProtocolWhisperer01 		   ##
 #############################################################################
+
+
+# In this, I was being tested of items in a list and getting a positive difference.
+
+from numpy import prod
+def find_difference(a, b):
+    return prod(a) - prod(b) if prod(a) > prod(b) else prod(b) - prod(a)
